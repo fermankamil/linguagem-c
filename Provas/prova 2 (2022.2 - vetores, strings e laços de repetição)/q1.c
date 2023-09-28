6f7cfcdff080a36494c421a5965dbb3475b88426
Rafael Mota <128419553+RafaelMotaCR@users.noreply.github.com>
2023-09-27 22:55:39 -0300

    Update q1.c

--- Provas/prova 2 (2022.2 - vetores, strings e laços de repetição)/q1.c
+++ Provas/prova 2 (2022.2 - vetores, strings e laços de repetição)/q1.c
@@ -1,36 +1,42 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <strings.h>
+
 int main(){
-	int c = 0, oco = 0, pos[20], i = 0, j = 0;
+	int c, pos[20], j;
+	int i = 0, ocorrencias = 0;
 	char palavra[50], frase[100];
+	
 	printf("> Digite uma frase: ");
 	gets(frase);
 	printf("> Digite uma palavra: ");
 	gets(palavra);
+	
 	for(j = 0, c = 0; frase[c] != '\0'; ++c){
+		if (j > 0 && frase [c] != palavra[j]){
+			j = 0;
+		}	
 		if(frase[c] == palavra[j]){
 			if(j == strlen(palavra) - 1){
-				j = 0;
-				oco++;
+				ocorrencias++;
 				pos[i] = c - (strlen(palavra) - 2);
 				i++;
-				j++;
+				j = 1;
 				}
-			else
+			else{
 				++j;
 			}
-		else if (j > 0 && frase [c] != palavra[j]){
-				j = 0;
-		}	
+		}
 	}
-	if(oco == 1){
-		printf("> A palavra %s aparece %d vez na frase digitada.\n", palavra, oco);
+	
+	if(ocorrencias == 1){
+		printf("> A palavra %s aparece %d vez na frase digitada.\n", palavra, ocorrencias);
 		printf("> A ocorrencia foi na posicao: %d.", pos[0]);
 	}
-	else if(oco == 0)
+	else if(!ocorrencias)
 		system("pause");
 	else{
-		printf("> A palavra %s aparece %d vezes na frase digitada.\n", palavra, oco);
+		printf("> A palavra %s aparece %d vezes na frase digitada.\n", palavra, ocorrencias);
 		printf("> As ocorrencias foram nas posicoes: ");
 		for(c = 0; c < i; ++c){
 			if(i == 1)
